2ad11edc4c9bdb257c63649aa7387e96c7461d71
•/ Mʀ. Aɴᴏɴ <51384033+noobanon@users.noreply.github.com>
2021-07-05 12:26:41 +0530

    Update cust_filters.py

--- marvel/modules/cust_filters.py
+++ marvel/modules/cust_filters.py
@@ -31,6 +31,19 @@ from marvel.modules.connection import connected
 HANDLER_GROUP = 10
 
 
+ENUM_FUNC_MAP = {
+	sql.Types.TEXT.value: dispatcher.bot.send_message,
+	sql.Types.BUTTON_TEXT.value: dispatcher.bot.send_message,
+	sql.Types.STICKER.value: dispatcher.bot.send_sticker,
+	sql.Types.DOCUMENT.value: dispatcher.bot.send_document,
+	sql.Types.PHOTO.value: dispatcher.bot.send_photo,
+	sql.Types.AUDIO.value: dispatcher.bot.send_audio,
+	sql.Types.VOICE.value: dispatcher.bot.send_voice,
+	sql.Types.VIDEO.value: dispatcher.bot.send_video,
+	sql.Types.VIDEO_NOTE.value: dispatcher.bot.send_video_note
+}
+
+
 def list_handlers(update, context):
     chat = update.effective_chat  # type: Optional[Chat]
     user = update.effective_user  # type: Optional[User]
@@ -79,107 +92,142 @@ def addnew_filter(update, chat_id, keyword, content, is_sticker, is_document, is
 
 # NOT ASYNC BECAUSE DISPATCHER HANDLER RAISED
 @user_admin
-def filters(update, context):
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
-    msg = update.effective_message  # type: Optional[Message]
-    args = msg.text.split(None, 1)  # use python's maxsplit to separate Cmd, keyword, and reply_text
-
-    conn = connected(context.bot, update, chat, user.id)
-    if not conn == False:
-        chat_id = conn
-        chat_name = dispatcher.bot.getChat(conn).title
+def filters(update: Update, context: CallbackContext):
+    bot =  context.bot
+    chat = update.effective_chat
+    user = update.effective_user
+    msg = update.effective_message
+    args = msg.text_markdown.split(
+        None, 1
+    )  # use python's maxsplit to separate Cmd, keyword, and reply_text
+    chat_id = update.effective_chat.id
+    if chat.type == "private":
+        chat_name = "local filters"
     else:
-        chat_id = update.effective_chat.id
-        if chat.type == "private":
-            chat_name = "local notes"
-        else:
-            chat_name = chat.title
+        chat_name = chat.title
 
-    if len(args) < 2:
+    if not msg.reply_to_message and len(args) < 2:
+        send_message(
+            update.effective_message,
+            "Please provide keyboard keyword for this filter to reply with!",
+        )
         return
 
-    extracted = split_quotes(args[1])
-    if len(extracted) < 1:
-        return
-    # set trigger -> lower, so as to avoid adding duplicate filters with different cases
-    keyword = extracted[0].lower()
-
-    is_sticker = False
-    is_document = False
-    is_image = False
-    is_voice = False
-    is_audio = False
-    is_video = False
-    buttons = []
-
-    # determine what the contents of the filter are - text, image, sticker, etc
-    if len(extracted) >= 2:
-        offset = len(extracted[1]) - len(msg.text)  # set correct offset relative to command + notename
-        content, buttons = button_markdown_parser(extracted[1], entities=msg.parse_entities(), offset=offset)
-        content = content.strip()
-        if not content:
-            msg.reply_text(tld(chat.id, "There is no note message - You can't JUST have buttons, you need a message to go with it!"))
+    if msg.reply_to_message:
+        if len(args) < 2:
+            send_message(
+                update.effective_message,
+                "Please provide keyword for this filter to reply with!",
+            )
             return
-
-    elif msg.reply_to_message and msg.reply_to_message.sticker:
-        content = msg.reply_to_message.sticker.file_id
-        is_sticker = True
-
-    elif msg.reply_to_message and msg.reply_to_message.document:
-        content = msg.reply_to_message.document.file_id
-        is_document = True
-
-    elif msg.reply_to_message and msg.reply_to_message.photo:
-        content = msg.reply_to_message.photo[-1].file_id  # last elem = best quality
-        is_image = True
-
-    elif msg.reply_to_message and msg.reply_to_message.audio:
-        content = msg.reply_to_message.audio.file_id
-        is_audio = True
-
-    elif msg.reply_to_message and msg.reply_to_message.voice:
-        content = msg.reply_to_message.voice.file_id
-        is_voice = True
-
-    elif msg.reply_to_message and msg.reply_to_message.video:
-        content = msg.reply_to_message.video.file_id
-        is_video = True
-
+        else:
+            keyword = context.args[0]
     else:
-        msg.reply_text(tld(chat.id, "You didn't specify what to reply with!"))
-        return
+        extracted = split_quotes(args[1])
+        if len(extracted) < 1:
+            return
+        # set trigger -> lower, so as to avoid adding duplicate filters with different cases
+        keyword = extracted[0].lower()
 
     # Add the filter
     # Note: perhaps handlers can be removed somehow using sql.get_chat_filters
-    add = addnew_filter(update, chat_id, keyword, content, is_sticker, is_document, is_image, is_audio, is_voice, is_video,
-                   buttons)
-    
     for handler in dispatcher.handlers.get(HANDLER_GROUP, []):
         if handler.filters == (keyword, chat_id):
-            dispatcher.remove_handler(handler, HANDLER_GROUP)            
-            if add is True:                
-                msg.reply_text(tld(chat.id, "Handler '{}' added in *{}*!").format(keyword, chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
+            dispatcher.remove_handler(handler, HANDLER_GROUP)
+
+    text, file_type, file_id = get_filter_type(msg)
+    if not msg.reply_to_message and len(extracted) >= 2:
+        offset = len(extracted[1]) - len(
+            msg.text
+        )  # set correct offset relative to command + notename
+        text, buttons = button_markdown_parser(
+            extracted[1], entities=msg.parse_entities(), offset=offset
+        )
+        
+        text = text.strip()
+        if not text:
+            send_message(
+                update.effective_message,
+                "There is no note message - You can't JUST have buttons, you need a message to go with it!",
+            )
+            return
+
+    elif msg.reply_to_message and len(args) >= 2:
+        if msg.reply_to_message.text:
+            text_to_parsing = msg.reply_to_message.text
+        elif msg.reply_to_message.caption:
+            text_to_parsing = msg.reply_to_message.caption
+        else:
+            text_to_parsing = ""
+        offset = len(
+            text_to_parsing
+        )  # set correct offset relative to command + notename
+        if len(msg.text.split(" ")) > 2:
+            text_to_parsing = msg.text_markdown.split(None, 2)[2]
+        text, buttons = button_markdown_parser(
+            text_to_parsing, entities=msg.parse_entities(), offset=offset
+        )
+        
+        text = text.strip()
+
+    elif not text and not file_type:
+        send_message(
+            update.effective_message,
+            "Please provide keyword for this filter reply with!",
+        )
+        return
+
+    elif msg.reply_to_message:
+        if msg.reply_to_message.text:
+            text_to_parsing = msg.reply_to_message.text
+        elif msg.reply_to_message.caption:
+            text_to_parsing = msg.reply_to_message.caption
+        else:
+            text_to_parsing = ""
+        offset = len(
+            text_to_parsing
+        )  # set correct offset relative to command + notename
+        text, buttons = button_markdown_parser(
+            text_to_parsing, entities=msg.parse_entities(), offset=offset
+        )
+        text = text.strip()
+        if (msg.reply_to_message.text or msg.reply_to_message.caption) and not text:
+            send_message(
+                update.effective_message,
+                "There is no note message - You can't JUST have buttons, you need a message to go with it!",
+            )
+            return
+
+    else:
+        send_message(update.effective_message, "Invalid filter!")
+        return
+
+    add = addnew_filter(update, chat_id, keyword, text, file_type, file_id, buttons)
+    # This is an old method
+    # sql.add_filter(chat_id, keyword, content, is_sticker, is_document, is_image, is_audio, is_voice, is_video, buttons)
+
+    if add is True:
+        send_message(
+            update.effective_message,
+            "Saved filter '{}' in *{}*!".format(keyword, chat_name),
+            parse_mode=telegram.ParseMode.MARKDOWN,
+        )
     raise DispatcherHandlerStop
+
 
 # NOT ASYNC BECAUSE DISPATCHER HANDLER RAISED
 @user_admin
-def stop_filter(update, context):
-    chat = update.effective_chat  # type: Optional[Chat]
-    user = update.effective_user  # type: Optional[User]
+def stop_filter(update: Update, context: CallbackContext):
     bot = context.bot
+    chat = update.effective_chat
+    user = update.effective_user
     args = update.effective_message.text.split(None, 1)
 
-    conn = connected(bot, update, chat, user.id)
-    if not conn == False:
-        chat_id = conn
-        chat_name = dispatcher.bot.getChat(conn).title
+    chat_id = update.effective_chat.id
+    if chat.type == "private":
+        chat_name = "local notes"
     else:
-        chat_id = update.effective_chat.id
-        if chat.type == "private":
-            chat_name = "local notes"
-        else:
-            chat_name = chat.title
+        chat_name = chat.title
 
     if len(args) < 2:
         return
@@ -187,16 +235,16 @@ def stop_filter(update, context):
     chat_filters = sql.get_chat_triggers(chat_id)
 
     if not chat_filters:
-        update.effective_message.reply_text(tld(chat.id, "No filters are active in {}!").format(chat_name))
+        update.effective_message.reply_text("No filters are active in *{}*!".format(chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
         return
 
     for keyword in chat_filters:
         if keyword == args[1]:
             sql.remove_filter(chat_id, args[1])
-            update.effective_message.reply_text(tld(chat.id, "Yep, I'll stop replying to that in *{}*.").format(chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
+            update.effective_message.reply_text("Yep, I'll stop replying to that in *{}*.".format(chat_name), parse_mode=telegram.ParseMode.MARKDOWN)
             raise DispatcherHandlerStop
 
-    update.effective_message.reply_text(tld(chat.id, "That's not a current filter - run /filters for all active filters."))
+    update.effective_message.reply_text("That's not a current filter - run /filters for all active filters.")
 
 
 
