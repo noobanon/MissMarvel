15055344fc96d75c5edaabd7a1a7b1951aea8e4c
•/ Mʀ. Aɴᴏɴ <51384033+noobanon@users.noreply.github.com>
2021-11-22 11:20:23 +0530

    Update chat_status.py

--- marvel/modules/helper_funcs/chat_status.py
+++ marvel/modules/helper_funcs/chat_status.py
@@ -14,6 +14,11 @@ from marvel import DEL_CMDS, SUDO_USERS, WHITELIST_USERS
 import marvel.modules.sql.admin_sql as admin_sql
 from marvel.modules.translations.strings import tld
 
+from marvel.mwt import MWT
+
+# stores admemes in memory for 10 min.	
+ADMIN_CACHE = TTLCache(maxsize=512, ttl=60 * 10, timer=perf_counter)	
+THREAD_LOCK = RLock()	
 
 def can_delete(chat: Chat, bot_id: int) -> bool:
     return chat.get_member(bot_id).can_delete_messages
@@ -32,15 +37,29 @@ def is_user_ban_protected(chat: Chat, user_id: int, member: ChatMember = None) -
 
 
 def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
-    if chat.type == 'private' \
-            or user_id in SUDO_USERS \
-            or chat.all_members_are_administrators or user_id in (777000, 1087968824, 1091139479):
+    if (
+        chat.type == "private"
+        or user_id in SUDO_USERS
+        or chat.all_members_are_administrators
+        or user_id in [777000, 1087968824]
+    ):  # Count telegram and Group Anonymous as admin
         return True
-
     if not member:
-        member = chat.get_member(user_id)
-    return member.status in ('administrator', 'creator')
+        with THREAD_LOCK:
+            # try to fetch from cache first.
+            try:
+                return user_id in ADMIN_CACHE[chat.id]
+            except KeyError:
+                # keyerror happend means cache is deleted,
+                # so query bot api again and return user status
+                # while saving it in cache for future useage...
+                chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
+                admin_list = [x.user.id for x in chat_admins]
+                ADMIN_CACHE[chat.id] = admin_list
 
+                return user_id in admin_list
+    else:
+        return member.status in ("administrator", "creator")
 
 def is_bot_admin(chat: Chat, bot_id: int, bot_member: ChatMember = None) -> bool:
     if chat.type == 'private' \
